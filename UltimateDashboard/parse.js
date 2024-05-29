0f11f94516707c84a3797b833493bc7761dd8503
Alzina <alexander.r.alzina@intel.com>
2024-05-29 15:37:17 -0700

    Update parse.js

--- UltimateDashboard/parse.js
+++ UltimateDashboard/parse.js
@@ -143,11 +143,11 @@ function query (p) {
             buildOpenItemBox(DATASETS.REQUEST)
             break
 
-          case SHARED_DATASETS.COS_ENTITY_1270:
+          case SHARED_DATASETS.COS_CEID_1270:
             PARSE_FUNCTIONS.COS_ENTITY(data.value)
             break
 
-          case SHARED_DATASETS.COS_ENTITY_1274:
+          case SHARED_DATASETS.COS_CEID_1274:
             PARSE_FUNCTIONS.COS_ENTITY(data.value)
             break
 
