44c6e60b8d62d68d5278a0f076fdcae3c019a760
aralzina <alexander.r.alzina@intel.com>
2025-06-23 15:08:06 -0700

    Update MainScript.js

--- PAM/JavaScript/MainScript.js
+++ PAM/JavaScript/MainScript.js
@@ -533,7 +533,7 @@ function checkAndQuery() {
         // add parameter value to DATASETS
         Object.keys(DATASETS).forEach(key => {
             if (DATASETS[key].COOKIE_NAME.length > 0) {
-                DATASETS[key].PARAMETER_VALUE = getCookie(DATASETS[key].COOKIE_NAME)
+                DATASETS[key].PARAMETER_VALUE = getCookie(DATASETS[key].COOKIE_NAME).join(',')
             }
         })
         queryData()
